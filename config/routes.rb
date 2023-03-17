223bc2b0659d2f45295bee1fc40ecfac4c64504d
git_LinexMugambi <linexmugambi254@gmail.com>
2023-03-17 17:00:45 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
 Rails.application.routes.draw do
-  get 'restaurant_pizzas/index'
-  get 'restaurant_pizzas/show'
-  get 'restaurant_pizzas/new'
-  get 'restaurant_pizzas/create'
-  get 'restaurant_pizzas/edit'
-  get 'restaurant_pizzas/update'
-  get 'restaurant_pizzas/destroy'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  get 'restaurants/edit'
-  get 'restaurants/update'
-  get 'restaurants/destroy'
-  get 'pizzas/index'
-  get 'pizzas/show'
-  get 'pizzas/new'
-  get 'pizzas/create'
-  get 'pizzas/edit'
-  get 'pizzas/update'
-  get 'pizzas/destroy'
+  resources :restaurant_pizzas, only: [:index, :create]
+  resources :restaurants, only: [:index, :show]
+  resources :pizzas, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
