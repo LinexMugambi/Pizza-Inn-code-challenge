fafebfb276e67811d819281032d133c9cd2a2a5e
git_LinexMugambi <linexmugambi254@gmail.com>
2023-03-17 16:58:06 +0300

    updated table

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_132159) do
-  create_table "pizza_restaurants", force: :cascade do |t|
-    t.integer "price"
-    t.integer "pizza_id", null: false
-    t.integer "restaurant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["pizza_id"], name: "index_pizza_restaurants_on_pizza_id"
-    t.index ["restaurant_id"], name: "index_pizza_restaurants_on_restaurant_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_135657) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurant_pizzas", force: :cascade do |t|
+    t.integer "price"
+    t.integer "pizza_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pizza_id"], name: "index_restaurant_pizzas_on_pizza_id"
+    t.index ["restaurant_id"], name: "index_restaurant_pizzas_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -35,6 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_132159) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "pizza_restaurants", "pizzas"
-  add_foreign_key "pizza_restaurants", "restaurants"
+  add_foreign_key "restaurant_pizzas", "pizzas"
+  add_foreign_key "restaurant_pizzas", "restaurants"
 end
